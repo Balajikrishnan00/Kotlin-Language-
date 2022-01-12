8f0a36560738dc61379b60ab39f735b76af68171
balaji <balajikrishnan00@gmail.com>
2022-01-12 19:46:17 +0300

    2 commit

---
+++ Kotlin Language Basics/src/01_Variables and Types/01_hello_world.kt
@@ -0,0 +1,5 @@
+package `01_Variables and Types`
+
+fun main(args:Array<String>) {
+    println("Hello world!")
+}
---
+++ Kotlin Language Basics/src/01_Variables and Types/02_variables.kt
@@ -0,0 +1,14 @@
+package `01_Variables and Types`
+
+fun main() {
+    val firstName="Balaji"
+    val lastName="Krishnan"
+    val fulName:String=firstName+lastName
+
+    println("full name :$fulName")
+    println("full name :"+fulName)
+    println("full name :"+firstName+lastName)
+
+
+
+}
